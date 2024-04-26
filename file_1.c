12dfcc7d394ad1390baa01b414a4b6f83e1d14db
RaagullKris <2020wa86653@wilp.bits-pilani.ac.in>
2024-04-26 19:28:45 +0530

    adding same function from main branch

---
+++ file_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+int number, number2, sum;
+printf("Enter two integers: ");
+sacnf("%d %d", &number1, &number2);
+//calculate thensum
+sum=number1 + number2;
+printf("%d + %d = %d", number1, number2, sum);
+return 0;
+}
+
