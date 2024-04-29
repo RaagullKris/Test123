fcaf4103b4f22e666c79730daae20bd6c3d3cfcf
RaagullKris <2020wa86653@wilp.bits-pilani.ac.in>
2024-04-29 17:36:47 +0530

    adding some function from main branch

--- file_1.c
+++ file_1.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 int main () {
-int number, number2, sum;
-printf("Enter two integers: ");
-sacnf("%d %d", &number1, &number2);
-//calculate thensum
-sum=number1 + number2;
-printf("%d + %d = %d", number1, number2, sum);
-return 0;
-}
-
+	int number1, number2, sum;
+	printf("Enter two integers:");
+	scanf("%d%d", &number1, &number2);
+	//calcukate the sum
+	sum = number1 + number2;
+	printf("%d + %d = %d", number1, number2, sum);
+	return 0;
+	<<<<<<< HEAD
+		int addfunc(int a, int b);
+	int c = a+b;
+	return c;
+	//function added in branch1 int func_add(int a, int b)
+	return a+b;
+	>>>>>branch1
+	
